2bd0bbce1b991863fd8cf95c68c490fbf2979a14
narendermanomay-svg <narender.manomay@gmail.com>
2026-06-22 01:24:51 +0530

    Duplicate key "caseStudy"

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -165,8 +165,7 @@ export default function Hero({ onNavigate }: HeroProps) {
             <div className="absolute top-1/2 left-1/2 ...">
             <div className="absolute -top-10 right-10 h-32 w-32 bg-[#cbdaa9]/20 rounded-full blur-2xl z-10"></div>
         </div>
-        </div>
-        </div>
+        
      </section>
   );
 }
